5dcc5d01db2b6722137771979400a34a65def8b8
이지윤 <83629615+jiyun0414@users.noreply.github.com>
2021-06-09 23:30:45 +0900

    Update 글자지우기.py

--- 글자지우기.py
+++ 글자지우기.py
@@ -2,4 +2,12 @@
 (html lang = "en")
 
 (heal)
-   (meta charset = "UTF
+   <meta charset = "UTF-8">
+   <meta http - equiv = "x-UA-Comatible" content = "IE - edge">
+   <meta name = "viewport" content = "width = device - width, initial - scale = 1.0">
+   <title>Document</title>
+</head>
+<body>
+<em>글자지우기</em>
+<body>
+</html?
